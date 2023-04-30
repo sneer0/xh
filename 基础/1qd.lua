bcef321ebfea1edb80a019bb5aabd5859af0bf62
嘲讽 <112706806+sneer0@users.noreply.github.com>
2023-04-30 22:33:35 +0800

    Update 1qd.lua

--- 基础/1qd.lua
+++ 基础/1qd.lua
@@ -1,6 +1,8 @@
 --首次启动♥♥♥
 
+task(500,function()--延迟开始
 local txt="zy1"--切换主页2
 io.open("/storage/emulated/0/Android/data/com.sneer.xhqll/qhzy.txt","w+"):write(txt):close()
 import '1_zy'--主页♥
 import '1_ssk'--搜索框♥
+end)--延迟结束
