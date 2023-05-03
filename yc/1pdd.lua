ba5ba4a54fb63334509bc6a5342379ca0acac2d0
嘲讽 <112706806+sneer0@users.noreply.github.com>
2023-05-03 22:21:59 +0800

    Update 1pdd.lua

--- yc/1pdd.lua
+++ yc/1pdd.lua
@@ -1,1 +1,2 @@
-https://mobile.yangkeduo.com/duo_cms_mall.html?pid=13588781_217250066&cpsSign=CM_210825_13588781_217250066_068df682af11e13f642e232dbe3a1108&duoduo_type=2
+设置顶栏标题("拼多多")
+加载网页("https://mobile.yangkeduo.com/duo_cms_mall.html?pid=13588781_217250066&cpsSign=CM_210825_13588781_217250066_068df682af11e13f642e232dbe3a1108&duoduo_type=2")
