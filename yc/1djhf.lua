f6c45afa7eebc4d07d3662b81cd5ac0d4a6261a9
嘲讽 <112706806+sneer0@users.noreply.github.com>
2023-05-03 22:19:57 +0800

    Update 1djhf.lua

--- yc/1djhf.lua
+++ yc/1djhf.lua
@@ -1,2 +1,2 @@
 设置顶栏标题("低价话费")
-加载网页("")
+加载网页("https://mobile.yangkeduo.com/duo_transfer_channel.html?pid=13588781_217250066&cpsSign=CC_210825_13588781_217250066_460302f59c1562603fb54e5e8d43ec45&duoduo_type=2&__page=duo_deposit")
