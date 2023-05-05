3c7495ef1cb389bef8f5ed991fe330f34ab2bf4f
嘲讽 <112706806+sneer0@users.noreply.github.com>
2023-05-05 11:48:47 +0800

    Update dggw.lua

--- gg/dggw.lua
+++ gg/dggw.lua
@@ -45,6 +45,7 @@ dbj=
                 layout_height='600dp',--卡片高度◆
                 CardBackgroundColor='#FFFFFF',--卡片背景颜色
                 layout_margin='14dp',--卡片边距
+                layout_marginTop='20dp',--卡片顶距
                 elevation='2dp',--阴影属性
                 radius='10dp',--卡片圆角
                 {
@@ -52,13 +53,13 @@ dbj=
                   layout_width='fill',--图片宽度
                   layout_height='600dp',--图片高度◆
                   scaleType='fitXY',--图片显示类型
-                  background='https://txc.gtimg.com/data/292760/2023/0401/34b99147b78a320c75e9e19f56f41984.jpeg',--背景颜色(图片路径)
+                  background='https://txc.gtimg.com/data/292760/2022/0816/135543be5f9ef863365b3dd029ae1ac1.png',--背景颜色(图片路径)
                 },
                 {
                   TextView,
                   layout_width='match_parent',--本文宽度
                   layout_height='30dp',--本文高度
-                  text='求路过的歌友给我露米叠点个关注吧，磕头了！',--文字2◆
+                  text='快来一起捡漏吧！！',--文字2◆
                   textSize='15sp',--本文大小
                   textColor='#FFFFFFFF',--本文颜色
                   background='#44000000',--本文背景
@@ -80,6 +81,7 @@ dbj=
                 layout_height='600dp',--卡片高度◆
                 CardBackgroundColor='#FFFFFF',--卡片背景颜色
                 layout_margin='14dp',--卡片边距
+                layout_marginTop='20dp',--卡片顶距
                 elevation='2dp',--阴影属性
                 radius='10dp',--卡片圆角
                 {
@@ -102,76 +104,7 @@ dbj=
                 },
               },
             },
-            {--●3●
-              LinearLayout,
-              orientation='vertical',--重力属性
-              layout_width='fill',--布局宽度
-              layout_height='fill',--布局高度
-              gravity='center',--宽度重力
-              id='tp3',--滑动id◆◆◆
-              {
-                CardView,--卡片控件
-                layout_width='fill',--卡片宽度
-                layout_height='600dp',--卡片高度◆
-                CardBackgroundColor='#FFFFFF',--卡片背景颜色
-                layout_margin='14dp',--卡片边距
-                elevation='2dp',--阴影属性
-                radius='10dp',--卡片圆角
-                {
-                  ImageView,--图片控件
-                  layout_width='fill',--图片宽度
-                  layout_height='600dp',--图片高度◆
-                  scaleType='fitXY',--图片显示类型
-                  background='https://txc.gtimg.com/data/292760/2022/0816/135543be5f9ef863365b3dd029ae1ac1.png',--背景颜色(图片路径)
-                },
-                {
-                  TextView,
-                  layout_width='match_parent',--本文宽度
-                  layout_height='30dp',--本文高度
-                  text='快来一起捡漏吧！！',--文字3◆
-                  textSize='15sp',--本文大小
-                  textColor='#FFFFFFFF',--本文颜色
-                  background='#44000000',--本文背景
-                  gravity='center',--宽度重力
-                  layout_gravity='bottom',--高度重力
-                },
-              },
-            },
-            {--●4●
-              LinearLayout,
-              orientation='vertical',--重力属性
-              layout_width='fill',--布局宽度
-              layout_height='fill',--布局高度
-              gravity='center',--宽度重力
-              id='tp4',--滑动id◆◆◆
-              {
-                CardView,--卡片控件
-                layout_width='fill',--卡片宽度
-                layout_height='600dp',--卡片高度◆
-                CardBackgroundColor='#FFFFFF',--卡片背景颜色
-                layout_margin='14dp',--卡片边距
-                elevation='2dp',--阴影属性
-                radius='10dp',--卡片圆角
-                {
-                  ImageView,--图片控件
-                  layout_width='fill',--图片宽度
-                  layout_height='600dp',--图片高度◆
-                  scaleType='fitXY',--图片显示类型
-                  background='https://txc.gtimg.com/data/292760/2022/0816/0fccb554d9aeb747b30bcc603182a8e0.png',--背景颜色(图片路径)
-                },
-                {
-                  TextView,
-                  layout_width='match_parent',--本文宽度
-                  layout_height='30dp',--本文高度
-                  text='功能超强的浏览器~',--文字2◆
-                  textSize='15sp',--本文大小
-                  textColor='#FFFFFFFF',--本文颜色
-                  background='#44000000',--本文背景
-                  gravity='center',--宽度重力
-                  layout_gravity='bottom',--高度重力
-                },
-              },
-            },
+            
             --结束♥♥♥♥♥
 
           },
@@ -185,15 +118,9 @@ webView.addView(loadlayout(dbj))--网络幽灵
 
 
 
---自动滑动图片♥♥♥♥♥
 tp1.onClick=function()--●1●
   dggw.showPage(0)--序号◆◆◆
-       import "android.content.Intent"
-       import "android.net.Uri"
-       url="https://b23.tv/N48rTmv"
-       viewIntent = Intent("android.intent.action.VIEW",Uri.parse(url))
-       activity.startActivity(viewIntent)
-       退出页面()
+  进入子页面("横竖屏",{标题="3,9包邮",链接="http://ocjxs.yhzu.cn/index.php?r=/npoint9"})
 end--结束
 tp2.onClick=function()--●3●
   dggw.showPage(1)--序号◆◆◆
@@ -201,23 +128,14 @@ tp2.onClick=function()--●3●
   弹出消息("请打开网易云APP联系推歌君")
   退出页面()
 end--结束
-tp3.onClick=function()--开始●2●
-  dggw.showPage(2)--序号◆◆◆
-  进入子页面("羊毛",{链接="http://ocjxs.yhzu.cn/index.php?r=/npoint9"})
-  退出页面()
-end--结束
-tp4.onClick=function()--●4●
-  dggw.showPage(3)--序号◆◆◆
-  进入子页面("浏览器",{标题="星火浏览器【请更新】",链接="https://lanzoui.com/b06sn34sd"})
-  退出页面()
-end--结束
-      
 
---图片停留时间♥♥♥♥♥
+
+
+--自动滑动图片♥♥♥♥♥
 local n=0 
 function djt()--监听
   task(2500,function()--几秒滑动1次
-    n=n+1 dggw.showPage(n%4)--4张图片◆◆◆
+    n=n+1 dggw.showPage(n%2)--2张图片◆◆◆
     djt()
   end)
 end 
